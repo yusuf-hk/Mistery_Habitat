ab5a11ea90a93752919ee27bc24a7a1ab54b0c47
Sander <Keedklang>
2020-02-11 16:25:20 +0100

    made interface layer and coins
    made interface layer and a coinsystem for collecting coins and diamonds

--- src/Objects/Common objects/Diamonds.gd
+++ src/Objects/Common objects/Diamonds.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 onready var anim_player: AnimationPlayer = $AnimationPlayer
 
-export var score: = 10
+export var score: = 100
 
 
 
@@ -11,8 +11,8 @@ func picked() -> void:
 	PlayerData.score += score
 	anim_player.play("Picked")
 	self.queue_free()
-
-
-func _on_Coins_body_entered(KinematicBody2D) -> void:
-	picked()
 	
+
+
+func _on_Diamond_body_entered(KinematicBody2D) -> void:
+	picked()
