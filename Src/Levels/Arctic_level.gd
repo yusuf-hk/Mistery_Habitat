5a4ddc92a8cfa820a4b711b7df04b96aa2e867ba
Sander <Keedklang>
2020-04-27 09:44:37 +0200

    mistake

--- Src/Levels/Arctic_level.gd
+++ Src/Levels/Arctic_level.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var animals_to_catch_arctic = 2
+onready var animals_to_catch_arctic = get_node("Animals").get_child_count()
 func _ready() -> void:
 	PlayerData.connect("animal", self, "is_task_completed")
 	get_node("Portal2D/ColorRect/ItemList/Label2").text = String(animals_to_catch_arctic)
